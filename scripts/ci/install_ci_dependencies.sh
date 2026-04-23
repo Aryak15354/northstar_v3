0ab719e7eaf0c6fba0280b44283c205f7620fb82
Aryak15354 <aryakghoshal@gmail.com>
2026-04-23 22:23:36 +0530

    Add missing Hypothesis test dependency

--- scripts/ci/install_ci_dependencies.sh
+++ scripts/ci/install_ci_dependencies.sh
@@ -8,7 +8,7 @@ PYTHON_BIN="${PYTHON_BIN:-python}"
 
 "$PYTHON_BIN" -m pip install --upgrade pip setuptools wheel
 "$PYTHON_BIN" -m pip install -r requirements.txt
-"$PYTHON_BIN" -m pip install pytest ruff packaging PyYAML
+"$PYTHON_BIN" -m pip install pytest ruff packaging PyYAML hypothesis
 "$PYTHON_BIN" - <<'PY'
 import pytest
 import yaml
